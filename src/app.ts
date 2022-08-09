a6725b9bb32d21298ecb22ceb5525b48a868c486
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-08-09 15:39:16 +0800

    feat: changed

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import { View, Text } from '@tarojs/components'
+
+import './app.scss'
+
+class App extends Component {
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  // this.props.children 是将要会渲染的页面
+  render() {
+    return ''
+  }
+}
+
+export default App
